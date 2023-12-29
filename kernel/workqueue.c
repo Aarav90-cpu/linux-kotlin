22fbf8d111d9c0e6e8823486a5be6590e3b5463a
Alfred Chen <cchalpha@gmail.com>
2023-12-29 11:06:33 +0000

    sched/alt: [Sync] 8639ecebc9b1 workqueue: Implement non-strict affinity scope for unbound workqueues

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1266,6 +1266,7 @@ static bool kick_pool(struct worker_pool *pool)
 
 	p = worker->task;
 
+#ifndef CONFIG_SCHED_ALT
 #ifdef CONFIG_SMP
 	/*
 	 * Idle @worker is about to execute @work and waking up provides an
@@ -1295,6 +1296,8 @@ static bool kick_pool(struct worker_pool *pool)
 		}
 	}
 #endif
+#endif /* !CONFIG_SCHED_ALT */
+
 	wake_up_process(p);
 	return true;
 }
